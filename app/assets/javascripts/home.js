4ce6c0e41d4906f44354cec919bf136119b32d69
Branden Staack <bsta520@gmail.com>
2017-03-24 16:43:03 -0400

    added animations for background, and icons with jquery

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,12 @@
 
-
 $(document).on('turbolinks:load', function(){
+//bounce icons when mouse over
   $('.btn').hover(function(){
+    $(this).toggleClass('pulsate-fwd');
+  });
+
+  $('.head').hover(function(){
+    $(this).toggleClass('bg-pan-top');
   });
 
 
