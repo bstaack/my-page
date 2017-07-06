63933391c7b5aa33221bac16241dc37bc873ea4c
Branden Staack <bsta520@gmail.com>
2017-07-06 17:43:14 -0400

    added website thumnails to projectsects folder and changed homepage background + removed ken burns effect

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,14 @@
 
 $(document).on('turbolinks:load', function(){
-//bounce icons when mouse over
+
+//bounce buttons when mouse over
   $('.btn').hover(function(){
     $(this).toggleClass('pulsate-fwd');
   });
 
-  $('.head').addClass('bg-pan-top');
-  $( '.head' ).hover(function() {
-    $( '.head' ).toggleClass('pause');
-  });
+  // $('.head').addClass('bg-pan-top');
+  // $( '.head' ).hover(function() {
+  //   $( '.head' ).toggleClass('pause');
+  // });
 
 });
